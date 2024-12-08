427a7eda318348bedfd8e018900de88420b9b8f1
dianak15 <missd151111@gmail.com>
2024-12-08 15:47:08 +0600

    Your commit message

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import folium
 
 
 # Define the FastAPI backend URL
-BACKEND_URL = "http://127.0.0.1:8000/diagnose"
+BACKEND_URL = "https://new-ai-app-5.onrender.com/diagnose"
 
 # Set your Groq API Key
 API_KEY = "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"  # Replace with your actual key
